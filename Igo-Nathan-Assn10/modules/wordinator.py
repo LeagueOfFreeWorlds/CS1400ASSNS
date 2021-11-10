a9cfd26644d9ecae2ee358a4b5acede1cc4e3203
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-11-10 11:57:45 -0700

    New work on wordinator.py

---
+++ Igo-Nathan-Assn10/modules/wordinator.py
@@ -0,0 +1,8 @@
+class Wordinator:
+    # Construct:
+    def __init__(self, wComplex1, wComplex2):
+        self.__wComplex1 = wComplex1.split()
+        self.__wComplex2 = wComplex2.split()
+    def min(self):
+
+
