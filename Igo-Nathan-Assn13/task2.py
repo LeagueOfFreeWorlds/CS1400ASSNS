8b8fa097c028cde8f87b571feb551a3dee0745f9
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-12-10 22:55:32 -0700

    Finished task2.py

--- Igo-Nathan-Assn13/task2.py
+++ Igo-Nathan-Assn13/task2.py
@@ -1,5 +1,4 @@
 from random import randint
-
 # Remember how to use this kind of variable?
 count = 0
 
@@ -28,6 +27,32 @@ def main():
 
     print("Total recursive calls: " + str(count))
 
+def aggienacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    else:
+        return n * (aggienacci(n - 3) + aggienacci(n - 2)) / aggienacci(n - 1)
+
+def binarySearch(nL, key):
+    global count
+    low = 0
+    high = len(nL) - 1
+    while high >= low:
+        count += 1
+        mid = (high + low) // 2
+        print(nL[low:high + 1])
+        if key == nL[mid]:
+            return mid
+        elif key < nL[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    print("Count:\t" + str(count))
+    return -1
 
 
 
