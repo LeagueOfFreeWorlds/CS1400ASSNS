fa916b077c4ec7e0c60b165e68d86663a8aace6b
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-12-11 09:10:26 -0700

    Finished Assn 13

--- Igo-Nathan-Assn13/modules/memoryboard.py
+++ Igo-Nathan-Assn13/modules/memoryboard.py
@@ -30,8 +30,6 @@ class MemoryBoard:
                 else:
                     string += ' '
 
-
-
                 string += ' | '
             string += '\n'
             string += '--------------------------------\n'
@@ -42,7 +40,7 @@ class MemoryBoard:
     def flipCard(self, xAx, yAx):
         self.__userSelection = self.__board[xAx][yAx]
         if self.isCardFlipped(xAx, yAx):
-            self.__userSelection = ''
+            self.__userSelection = 'A'
             self.getBoard()
         else:
             self.getBoard()
@@ -54,9 +52,8 @@ class MemoryBoard:
             return False
         else:
             return True
-    ## TBD - Please go back to this:
     def isMatch(self, pos1, pos2):
-        if self.__board[pos1] == board[pos2]:
+        if self.__board[pos1] == self.__board[pos2]:
             return True
         else:
          return False
